53c704c53e2fc680c3eacd89b98b4f62e3b3d32a
Joshua Tseng <joshuatseng0233@gmail.com>
2025-01-09 18:29:53 +0800

    Add files via upload
    minmaxheap

--- min_max_heap.c
+++ min_max_heap.c
@@ -1,5 +1,5 @@
 //
-// Created by Hding49 on 2024/12/27.
+// Created by Hding49 on 2025/1/3.
 //
 
 
@@ -17,12 +17,11 @@ typedef struct element {
     int key;
 } element;
 
-element heap[MAX_ELEMENTS + 1];
+element heap[MAX_ELEMENTS];
 
 #define ELE_EMPTY (element) {INT_MIN}
 
-int last = 1;
-int arrayLength = 0;
+int n = 0;
 
 
 void swap(void * a, void * b, size_t size) {
@@ -41,162 +40,126 @@ element createData(int key) {
 }
 
 
-int maxPartner(int i) {
-    int j = i + (1 << (int) (floor(log2(i)) - 1));
-    if (j > arrayLength) j /= 2;
-    return j;
+int isMinLevel(int i) {
+    return (int) (floor(log2(i))) % 2 == 0;
 }
 
 
-int minPartner(int j) {
-    return j - (1 << (int) (floor(log2(j)) - 1));
-}
-
-
-bool isMinNode(int i) {
-    return i + 1 < 3 * (1 << (int) (floor(log2(i + 1)) - 1));
-}
-
-
-void swim(int i, bool is_min) {
-    int item = heap[i].key;
-
-    while (true) {
-        if (is_min) {
-            if ((i != 1) && item < heap[(i + 1) / 2 - 1].key) {
-                heap[i] = heap[(i + 1) / 2 - 1];
-                i = (i + 1) / 2 - 1;
-            } else {
-                break;
-            }
-        } else {
-            if ((i != 2) && item > heap[(i + 1) / 2 - 1].key) {
-                heap[i] = heap[(i + 1) / 2 - 1];
-                i = (i + 1) / 2 - 1;
-            } else {
-                break;
-            }
-        }
-    }
-    heap[i].key = item;
-}
-
-
-void insert(element data) {
-    if (arrayLength == MAX_ELEMENTS) return;
-
-    heap[last] = data;
-
-    if (arrayLength != 0) {
-        bool is_min = isMinNode(last);
-        int k = is_min ? maxPartner(last + 1) - 1 : minPartner(last + 1) - 1;
-        bool condition = is_min ? heap[last].key > heap[k].key : heap[last].key < heap[k].key;
+void verifyMinMax(int p, element e, bool is_min) {
+    int gp = p / 4;
+    while (gp != 0) {
+        bool condition = is_min ? e.key < heap[gp].key : e.key > heap[gp].key;
         if (condition) {
-            swap(&heap[last], &heap[k], sizeof(heap[last]));
-            swim(k, !is_min);
-        } else swim(last, is_min);
-    }
-
-    last++;
-    arrayLength++;
-}
-
-
-element delete(bool is_min) {
-    int target;
-    if (is_min) {
-        if (arrayLength == 0) return ELE_EMPTY;
-        target = 1;
-    } else {
-        if (arrayLength < 2) return ELE_EMPTY;
-        target = 2;
-    }
-    element item = heap[target];
-    int child = (target + 1) * 2 - 1;
-    while (child <= arrayLength) {
-        if (child < arrayLength) {
-            bool condition = is_min ? (heap[child].key > heap[child + 1].key) : (heap[child].key < heap[child + 1].key);
-            child = condition ? child + 1 : child;
+            heap[p] = heap[gp];
+            p = gp;
+            gp /= 4;
+        } else {
+            gp = 0;
         }
-
-        heap[target] = heap[child];
-        target = child;
-        child = (child + 1) * 2 - 1;
     }
-
-    heap[target] = heap[arrayLength];
-    last--;
-    arrayLength--;
-    int k = is_min ? maxPartner(target + 1) - 1 : minPartner(target + 1) - 1;
-    bool condition = is_min ? heap[target].key > heap[k].key : heap[target].key < heap[k].key;
-    if (condition) {
-        swap(&heap[target], &heap[k], sizeof(heap[target]));
-        swim(k, !is_min);
-    } else swim(target, is_min);
-    return item;
+    heap[p] = e;
 }
 
 
-element deleteMin() {
-    return delete(true);
+void insert(element e) {
+    int p;
+    if (n == MAX_ELEMENTS - 1) return;
+
+    n++;
+    p = n / 2;
+
+    if (p == 0) heap[1] = e;
+    else {
+        if (isMinLevel(p)) {
+            if (e.key < heap[p].key) {
+                heap[n] = heap[p];
+                verifyMinMax(p, e, true);
+            } else verifyMinMax(n, e, false);
+        } else {
+            if (e.key > heap[p].key) {
+                heap[n] = heap[p];
+                verifyMinMax(p, e, false);
+            } else verifyMinMax(n, e, true);
+        }
+    }
 }
 
 
-element deleteMax() {
-    return delete(false);
+int findChildGrandChild(int i, bool is_min) {
+    int k = i * 2;
+    if (k > n) return -1;
+    if (k + 1 <= n) {
+        bool condition = is_min ? heap[k].key > heap[k + 1].key : heap[k].key < heap[k + 1].key;
+        k = condition ? k + 1 : k;
+    }
+    if (i * 4 <= n) {
+        for (int j = i * 4; j <= n && j < i * 8; j++) {
+            bool condition = is_min ? heap[k].key > heap[j].key : heap[k].key < heap[j].key;
+            k = condition ? j : k;
+        }
+    }
+    return k;
 }
 
 
+void delete(element * e, bool is_min) {
+    int i, j, k, p;
+    element x, t;
+    bool not_done;
+    if (n == 0) return;
 
-void printLevelOrder() {
-    if (arrayLength == 0) return;
-    for (int i = 1; i <= arrayLength; i++) {
+    *e = heap[1];
+    x = heap[n];
+    n--;
+    not_done = true;
+    i = 1;
+    j = n / 2;
+
+    while (not_done && i <= j) {
+        if ((k = findChildGrandChild(i, is_min)) == -1) break;
+        if (x.key <= heap[k].key) not_done = false;
+        else {
+            heap[i] = heap[k];
+            if (k <= 2 * i + 1) {
+                not_done = false;
+            } else {
+                p = k / 2;
+                if (x.key > heap[p].key) {
+                    t = heap[p];
+                    heap[p] = x;
+                    x = t;
+                }
+            }
+            i = k;
+        }
+    }
+    heap[i] = x;
+}
+
+
+void printHeap() {
+    for (int i = 1; i <= n; i++) {
         printf("%d ", heap[i].key);
     }
     printf("\n");
 }
+
+
+
 int main() {
-    char command[20];
-    printf("Commands:\n");
-    printf("insert <key>: Insert a key into the heap\n");
-    printf("deleteMin: Delete the minimum element\n");
-    printf("deleteMax: Delete the maximum element\n");
-    printf("print: Print the heap in level order\n");
-    printf("exit: Exit the program\n");
+//    int array[] = {7, 70, 40, 30, 9, 10, 15, 45, 50, 30, 20, 12};
 
-    while (true) {
-        printf("Enter command: ");
-        scanf("%s", command);
+    int array[] = {5, 30, 26, 10, 16, 20, 28, 12, 14, 25, 21, 22};
 
-        if (strcmp(command, "insert") == 0) {
-            int key;
-            scanf("%d", &key);
-            insert(createData(key));
-            printf("Inserted %d into the heap.\n", key);
-        } else if (strcmp(command, "deleteMin") == 0) {
-            element minElement = deleteMin();
-            if (minElement.key == INT_MIN) {
-                printf("Heap is empty. Cannot delete min.\n");
-            } else {
-                printf("Deleted Min: %d\n", minElement.key);
-            }
-        } else if (strcmp(command, "deleteMax") == 0) {
-            element maxElement = deleteMax();
-            if (maxElement.key == INT_MIN) {
-                printf("Heap is empty. Cannot delete max.\n");
-            } else {
-                printf("Deleted Max: %d\n", maxElement.key);
-            }
-        } else if (strcmp(command, "print") == 0) {
-            printf("Heap in Level Order: ");
-            printLevelOrder();
-        } else if (strcmp(command, "exit") == 0) {
-            printf("Exiting program.\n");
-            return 0;
-        } else {
-            printf("Invalid command. Please try again.\n");
-        }
+    for (int i = 0; i < 12; i++) {
+        heap[++n].key = array[i];
     }
 
-    return 0;
+    printHeap();
+
+    element e;
+    delete(&e, true);
+
+    printHeap();
+
 }
